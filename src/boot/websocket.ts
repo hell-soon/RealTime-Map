a265c421fd44f172fc0e5409a23fb7ba8969bebc
hellsoon <pmaks2365@gmail.com>
2025-06-26 17:31:50 +0300

    feat: socket

--- src/boot/websocket.ts
+++ src/boot/websocket.ts
@@ -1,20 +1,20 @@
 import { boot } from 'quasar/wrappers'
-import { useWebSocket } from 'src/composables/useWebSocket'
-import { useAuthStore } from 'src/stores/auth.store'
-import { watch } from 'vue'
+// import { useWebSocket } from 'src/composables/useWebSocket'
+// import { useAuthStore } from 'src/stores/auth.store'
+// import { watch } from 'vue'
 
 export default boot(() => {
-  const { connect, disconnect } = useWebSocket()
-  const authStore = useAuthStore()
+  // const { connect, disconnect } = useWebSocket()
+  // const authStore = useAuthStore()
 
-  watch(() => authStore.token, (newToken, oldToken) => {
-    if (newToken) {
-      connect()
-    }
-    else if (oldToken && !newToken) {
-      disconnect()
-    }
-  }, {
-    immediate: true,
-  })
+  // watch(() => authStore.token, (newToken, oldToken) => {
+  //   if (newToken) {
+  //     connect()
+  //   }
+  //   else if (oldToken && !newToken) {
+  //     disconnect()
+  //   }
+  // }, {
+  //   immediate: true,
+  // })
 })
